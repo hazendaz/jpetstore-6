fe2612a62dce97d190d8e1d2f98aa4e90d46f0c2
Jeremy Landis <jeremylandis@hotmail.com>
2025-04-27 23:10:40 -0400

    jakarta wip

--- src/main/java/org/mybatis/jpetstore/web/actions/OrderActionBean.java
+++ src/main/java/org/mybatis/jpetstore/web/actions/OrderActionBean.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2010-2022 the original author or authors.
+ *    Copyright 2010-2025 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -15,11 +15,11 @@
  */
 package org.mybatis.jpetstore.web.actions;
 
+import jakarta.servlet.http.HttpSession;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
-import javax.servlet.http.HttpSession;
 
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
